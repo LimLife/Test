6d015232013ee649cd4229e3f033d91eb4c0e6fe
LimLife <LimFobos@gamil.com>
2021-08-20 14:57:34 +0300

    First

---
+++ PrefabDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDown : MonoBehaviour
+{
+    [SerializeField] private float speedPrefab = 1f;
+    [SerializeField] private float sacleTime;
+   
+    // Update is called once per frame
+    
+    void Update()
+    {
+        while (true)
+        {
+            sacleTime += 0.25f;
+        }
+        transform.Translate(Vector3.down * Time.deltaTime * sacleTime);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace Floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] g = { 1, 2, 3, 4, 5, 6 };
+          int r =  Primer(g);
+            Console.WriteLine(r);
+        }
+        public static int Primer(int [] marks)
+        {
+            int rezult = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                double x = marks.Sum();
+                int integer = (int)Math.Round(x);
+                 rezult = integer / marks.Length;
+            }
+Console.WriteLine("Hello test Git Bash");
+          return rezult;
+        }
+    }
+}
